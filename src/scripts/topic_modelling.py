3871c92abdd39eb84d381d1a95d9a67063726289
ignacio <ignacioscavuzzo14@gmail.com>
2024-11-03 16:07:05 -0300

    bertopic submodels

--- src/scripts/topic_modelling.py
+++ src/scripts/topic_modelling.py
@@ -1,5 +1,9 @@
 from bertopic import BERTopic
 from sentence_transformers import SentenceTransformer
+from umap import UMAP
+from hdbscan import HDBSCAN
+from sklearn.feature_extraction.text import CountVectorizer
+from bertopic.vectorizers import ClassTfidfTransformer
 
 def apply_bertopic(sequences, model, reduced_topics):
     """
@@ -16,11 +20,19 @@ def apply_bertopic(sequences, model, reduced_topics):
     embedding_model = SentenceTransformer(model)
     embeddings = embedding_model.encode(sequences, show_progress_bar=True)
     
-    topic_model = BERTopic()
+    umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric="cosine", random_state=42)
+    hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+    vectorizer_model = CountVectorizer()
+    ctfidf_model = ClassTfidfTransformer()
 
-    topic_model.fit_transform(sequences, embeddings)
+    topic_model = BERTopic(umap_model=umap_model, embedding_model=embedding_model, hdbscan_model=hdbscan_model, vectorizer_model=vectorizer_model, ctfidf_model=ctfidf_model)
+
+    topics, _ = topic_model.fit_transform(sequences, embeddings)
     
     topic_model.reduce_topics(sequences, nr_topics=reduced_topics)
+    
+    # similar_topics, similarity = topic_model.find_topics("price", top_n=5)
+    # print(topic_model.get_topic(similar_topics[0]))
 
     return topic_model
 
